cbd6ced90c5a0272b8db4676c501ed80b399aa04
Ling Wang <an00na@gmail.com>
2013-02-21 15:42:52 -0600

    Release of version 0.0.2

--- code/letterpress.py
+++ code/letterpress.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version_info__ = (0, 0, 1)
+__version_info__ = (0, 0, 2)
 __version__ = '.'.join(map(str, __version_info__))
 __author__ = "Ling Wang"
 
